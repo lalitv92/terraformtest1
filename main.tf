4de830d4730ecbf7db4a22f149f29108f23867cd
Lalit vikram singh <lalitv92@gmail.com>
2019-01-28 15:19:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "google_compute_global_forwarding_rule" "http" {
   project    = "${var.project}"
   count      = "${var.http_forward ? 1 : 0}"
   name       = "${var.name}"
+  target     = "newtttttt"
   ip_address = "35.196.244.41"
   port_range = "9200"
 
