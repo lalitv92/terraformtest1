d9139635edab5f0ec31b2beba4c7e34372bf1842
Lalit vikram singh <lalitv92@gmail.com>
2019-01-28 14:51:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ resource "google_compute_global_forwarding_rule" "http" {
   target     = "${google_compute_target_http_proxy.default.self_link}"
   ip_address = "35.196.244.41"
   port_range = "9200"
-  depends_on = ["google_compute_global_address.default"]
+
 }
